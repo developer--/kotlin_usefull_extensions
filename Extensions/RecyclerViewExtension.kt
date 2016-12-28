0f8a011b9e5e908be4bd2eb303b59e513397a59c
Your Name <jemo.mgebrishvili@gmail.com>
2016-12-28 21:43:00 +0400

    recyclerView auto notify extension

---
+++ Extensions/RecyclerViewExtension.kt
@@ -0,0 +1,18 @@
+fun <T> RecyclerView.Adapter<*>.autoNotify(oldList: List<T>, newList: List<T>, compare: (T, T) -> Boolean) {
+    val diff = DiffUtil.calculateDiff(object : DiffUtil.Callback() {
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return compare(old[oldItemPosition], newList[newItemPosition])
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition] == newList[newItemPosition]
+        }
+
+        override fun getOldListSize() = oldList.size
+
+        override fun getNewListSize() = newList.size
+    })
+
+    diff.dispatchUpdatesTo(this)
+}
