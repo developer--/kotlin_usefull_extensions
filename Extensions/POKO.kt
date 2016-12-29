c292f1debe981362ef5cabe2f1b76fc46bd23c6e
Jemo Mgebrishvili <jemo.mgebrishvili@gmail.com>
2016-12-29 15:30:06 +0400

    Update POKO.kt

--- Extensions/POKO.kt
+++ Extensions/POKO.kt
@@ -1,10 +1,10 @@
 /**
-/ check if any view is not null
+/* check if any view is not null
 /*
 fun Any?.isNotNull(): Boolean = this != null
 
 
 /**
-/ check if view is null
+/* check if view is null
 /*
 fun Any?.isNull(): Boolean = this == null
