97f6587149450d0713c12795864077c1038f999b
Jemo Mgebrishvili <jemo.mgebrishvili@gmail.com>
2017-04-06 11:42:57 +0400

    Update POKO.kt

--- Extensions/POKO.kt
+++ Extensions/POKO.kt
@@ -8,3 +8,8 @@ fun Any?.isNotNull(): Boolean = this != null
 * check if view is null
 */
 fun Any?.isNull(): Boolean = this == null
+
+/**
+* check if the string isn't null or empty
+*/
+fun String?.isNotNullOrEmpty() = !isNullOrEmpty()
