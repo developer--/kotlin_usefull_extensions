96bfc311456c4211c88aa40f8263101d13554a1a
Jemo Mgebrishvili <jemo.mgebrishvili@geolab.edu.ge>
2017-07-19 11:43:27 +0400

    Update AnimationExtension.kt

--- Extensions/AnimationExtension.kt
+++ Extensions/AnimationExtension.kt
@@ -4,3 +4,35 @@ inline fun Animation.setAnimationListener(
     listener.func()
     setAnimationListener(listener)
 }
+
+
+class __AnimationListener : Animation.AnimationListener {
+    private var _onAnimationRepeat: ((animation: Animation?) -> Unit)? = null
+    private var _onAnimationEnd: ((animation: Animation?) -> Unit)? = null
+    private var _onAnimationStart: ((animation: Animation?) -> Unit)? = null
+
+    override fun onAnimationRepeat(animation: Animation?) {
+        _onAnimationRepeat?.invoke(animation)
+    }
+
+    fun onAnimationRepeat(func: (animation: Animation?) -> Unit) {
+        _onAnimationRepeat = func
+    }
+
+    override fun onAnimationEnd(animation: Animation?) {
+        _onAnimationEnd?.invoke(animation)
+    }
+
+    fun onAnimationEnd(func: (animation: Animation?) -> Unit) {
+        _onAnimationEnd = func
+    }
+
+    override fun onAnimationStart(animation: Animation?) {
+        _onAnimationStart?.invoke(animation)
+    }
+
+    fun onAnimationStart(func: (animation: Animation?) -> Unit) {
+        _onAnimationStart = func
+    }
+
+}
